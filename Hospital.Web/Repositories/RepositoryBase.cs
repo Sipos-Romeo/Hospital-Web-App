f2bb0c00b5531e87d466babafc0aaa080419d28d
Sipos-Romeo <siposromeo06@gmail.com>
2024-10-24 18:14:46 +0300

    Worked on repository

---
+++ Hospital.Web/Repositories/RepositoryBase.cs
@@ -0,0 +1,45 @@
+﻿using Hospital.Repositories;
+using Hospital.Web.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace Hospital.Web.Repositories
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected ApplicationDbcontext ApplicationDbcontext { get; set; }
+
+        public RepositoryBase(ApplicationDbcontext ApplicationDbcontext)
+        {
+            this.ApplicationDbcontext = ApplicationDbcontext;
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return this.ApplicationDbcontext.Set<T>();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return this.ApplicationDbcontext.Set<T>().Where(expression);
+        }
+        public T GetByCondition(Expression<Func<T, bool>> expression)
+        {
+            return this.ApplicationDbcontext.Set<T>().Where(expression).FirstOrDefault();
+        }
+
+        public void Create(T entity)
+        {
+            this.ApplicationDbcontext.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            this.ApplicationDbcontext.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            this.ApplicationDbcontext.Set<T>().Remove(entity);
+        }
+    }
+}
